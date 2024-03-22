f71ed8e4368ab3bf236f67166cb1ec9e45c8a48e
Bl4ck-Kurik <123986218+Bl4ck-Kurik@users.noreply.github.com>
2024-03-22 12:58:10 +0100

    Calender I-Frame style changes

--- src/components/CalendarComponent.vue
+++ src/components/CalendarComponent.vue
@@ -1,14 +1,21 @@
 <template>
-    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FBerlin&showTitle=1&showNav=1&showPrint=0&showTabs=0&showCalendars=0&showTz=0&src=Y2M2YmQwYjY4NDEyODI0MGNhNjUyZjUyODU3NGZjYjNkOWE1YmE0Njg2ZTA1MWE1YjhkNTAzNzc2NTY1NDYzYkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23D50000" frameborder="0" class="calendar-iframe"></iframe>
-</template>
-
+  <div class="calendar-wrapper">
+    <iframe class="calendar-iframe" frameborder="0" src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23121212&ctz=Europe%2FBerlin&showTitle=1&showNav=1&showPrint=0&showTabs=0&showCalendars=0&showTz=0&src=Y2M2YmQwYjY4NDEyODI0MGNhNjUyZjUyODU3NGZjYjNkOWE1YmE0Njg2ZTA1MWE1YjhkNTAzNzc2NTY1NDYzYkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23D50000"></iframe>
+  </div></template>
 <script setup>
 
 </script>
 
 <style>
+.calendar-wrapper {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  border-radius: 10px;
+  overflow: hidden;
+}
+
 .calendar-iframe {
-    width: 100%;
-    height: 600px;
+  width: 100%;
+  height: 600px;
+  border: none;
 }
 </style>
